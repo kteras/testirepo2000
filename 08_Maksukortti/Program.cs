e98de197230b633671e7312044a6f51d20e2df5a
Karri Teras <karri.teras@gradia.com>
2021-08-23 09:51:51 +0300

    08_6_Maksukortti esimerkkitoteutus.

--- 08_Maksukortti/Program.cs
+++ 08_Maksukortti/Program.cs
@@ -6,10 +6,33 @@ namespace _08_Maksukortti
     {
         static void Main(string[] args)
         {
-            Maksukortti kortti = new Maksukortti(10);
-            Console.WriteLine("Pekka: " + kortti); 
-            kortti.LataaRahaa(-15);
-            Console.WriteLine("Pekka: " + kortti); 
+            //Luo Pekan kortti. Kortin alkusaldo on 20 euroa
+            Maksukortti pekanKortti = new Maksukortti(20); 
+            //Luo Matin kortti. Kortin alkusaldo on 30 euroa
+            Maksukortti matinKortti = new Maksukortti(30);
+            //Pekka syö maukkaasti
+            pekanKortti.SyoMaukkaasti();
+            //Matti syö edullisesti
+            matinKortti.SyoEdullisesti();
+            //Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+            System.Console.WriteLine("Pekka: "+pekanKortti);
+            System.Console.WriteLine("Matti: "+matinKortti);
+            //Pekka lataa rahaa 20 euroa
+            pekanKortti.LataaRahaa(20);
+            //Matti syö maukkaasti
+            matinKortti.SyoMaukkaasti();
+            //Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+            System.Console.WriteLine("Pekka: "+pekanKortti);
+            System.Console.WriteLine("Matti: "+matinKortti);
+            //Pekka syö edullisesti
+            pekanKortti.SyoEdullisesti();
+            //Pekka syö edullisesti
+            pekanKortti.SyoEdullisesti();
+            //Matti lataa rahaa 50 euroa
+            matinKortti.LataaRahaa(50);
+            //Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+            System.Console.WriteLine("Pekka: "+pekanKortti);
+            System.Console.WriteLine("Matti: "+matinKortti);
         }
     }
 }
